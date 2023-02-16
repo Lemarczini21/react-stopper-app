724581fd0badd0a0928282ff2b07adaa9c6bad91
Marcin Gwizdek <94224709+Lemarczini@users.noreply.github.com>
2023-02-16 15:31:15 +0100

    fix bugs

--- src/components/Stopper/Stopper.js
+++ src/components/Stopper/Stopper.js
@@ -1,11 +1,11 @@
 import styles from './Stopper.module.scss';
 
-const Stoper = (props) => {
+const Stoper = ({time}) => {
   const formatTime = (miliseconds) => {
-    let ms = Math.floor(miliseconds % 100);
-    let seconds = Math.floor((miliseconds / 100) % 60);
-    let minutes = Math.floor((miliseconds / (100 * 60)) % 60);
-    let hours = Math.floor((miliseconds / (100 * 60 * 60)) % 24);
+    let ms = Math.floor(miliseconds);
+    let seconds = Math.floor((miliseconds / 1000) % 60);
+    let minutes = Math.floor((miliseconds / (1000 * 60)) % 60);
+    let hours = Math.floor((miliseconds / (1000 * 60 * 60)) % 24);
 
     hours = String(hours).padStart(2, '0');
     minutes = String(minutes).padStart(2, '0');
@@ -13,9 +13,10 @@ const Stoper = (props) => {
     ms = String(miliseconds).padStart(1, '0').slice(-3);
 
     return `${hours}:${minutes}:${seconds}.${ms}`;
+
   };
 
-  return <div className={styles.stopper}>{formatTime(props.time)}</div>;
+  return <div className={styles.stopper}>{formatTime(time)}</div>;
 };
 
 export default Stoper;
